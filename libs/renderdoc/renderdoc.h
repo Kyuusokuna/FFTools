ea93a4c62c29dac25e500bc09adb3bb18ce97c68
Sven Balzer <4653051+Kyuusokuna@users.noreply.github.com>
2022-03-17 16:50:07 +0100

    refactor renderdoc header

--- libs/renderdoc/renderdoc.h
+++ libs/renderdoc/renderdoc.h
@@ -1,8 +1,23 @@
 #pragma once
+#include <Windows.h>
+#include "renderdoc_app.h"
 
 namespace Renderdoc {
-    void init();
+    RENDERDOC_API_1_4_0 *rdoc_api = NULL;
 
-    void start_capture(void *device);
-    void stop_capture(void *device);
+    inline void init() {
+        if (HMODULE mod = GetModuleHandleA("renderdoc.dll")) {
+            pRENDERDOC_GetAPI RENDERDOC_GetAPI = (pRENDERDOC_GetAPI)GetProcAddress(mod, "RENDERDOC_GetAPI");
+            if (RENDERDOC_GetAPI)
+                RENDERDOC_GetAPI(eRENDERDOC_API_Version_1_1_2, (void **)&rdoc_api);
+        }
+    }
+
+    inline void start_capture(void *device) {
+        if (rdoc_api) rdoc_api->StartFrameCapture(device, NULL);
+    }
+
+    inline void stop_capture(void *device) {
+        if (rdoc_api) rdoc_api->EndFrameCapture(device, NULL);
+    }
 }
